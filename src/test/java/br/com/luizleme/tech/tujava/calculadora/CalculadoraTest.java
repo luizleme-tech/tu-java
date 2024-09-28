65528bdf22ad1b461f221f261573512fc0b9e968
oluizleme <o.luizleme@gmail.com>
2024-09-28 20:05:27 -0300

    feat(main): Implementado o teste validando o contrário do esperado

--- src/test/java/br/com/luizleme/tech/tujava/calculadora/CalculadoraTest.java
+++ src/test/java/br/com/luizleme/tech/tujava/calculadora/CalculadoraTest.java
@@ -3,6 +3,7 @@ package br.com.luizleme.tech.tujava.calculadora;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class CalculadoraTest {
 
@@ -17,6 +18,17 @@ public class CalculadoraTest {
 
 		//ASSERT
 		assertEquals(7, resultado,"a soma de 1 + 6 deve ser igual 7");
+	}
 
+	@Test
+	void deveValidarSeASomaDeDoisNumerosNaoEhIgualAoValorInformado() {
+		//ARRENGE
+		Calculadora calculadora = new Calculadora();
+
+		//ACT
+		int resultado = calculadora.somar(1, 6);
+
+		//ASSERT
+		assertNotEquals(8, resultado, "a soma de 1 + 6 não deve ser igual a 8");
 	}
 }
