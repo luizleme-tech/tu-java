c8a9ecab2eb4c9a4f1fecc587279457f963c7887
oluizleme <o.luizleme@gmail.com>
2024-09-28 19:42:32 -0300

    chore(main): Inicialização do Projeto

---
+++ src/main/java/br/com/luizleme/tech/tujava/TujavaApplication.java
@@ -0,0 +1,13 @@
+package br.com.luizleme.tech.tujava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TujavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TujavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/luizleme/tech/tujava/TujavaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.luizleme.tech.tujava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TujavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
