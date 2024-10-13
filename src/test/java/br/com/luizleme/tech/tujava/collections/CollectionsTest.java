5d13f5c2f533c696e703b788ee3f6b18286637db
Luiz Henrique Leme <luizleme.tech@gmail.com>
2024-10-13 13:40:30 -0300

    feat(testes-fluentes-com-assertJ): Implementado teste de validacao de Set  com assertJ

--- src/test/java/br/com/luizleme/tech/tujava/collections/CollectionsTest.java
+++ src/test/java/br/com/luizleme/tech/tujava/collections/CollectionsTest.java
@@ -3,7 +3,9 @@ package br.com.luizleme.tech.tujava.collections;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class CollectionsTest {
 	@Test
@@ -15,5 +17,15 @@ public class CollectionsTest {
 				.containsExactly("Maria", "Joana", "Augusta")
 				.doesNotContain("Marta")
 				.containsSequence("Joana", "Augusta");
+	}
+
+	@Test
+	void deveValisarUsandoSet() {
+		Set<String> cafes = new HashSet<>(List.of("Expresso", "Cappuccino", "Mocha", "Latte", "Americano", "Macchiato", "Filtrado"));
+		Assertions.assertThat(cafes)
+				.hasSize(7)
+				.contains("Filtrado")
+				.doesNotContain("Pingado")
+				.containsOnly("Expresso", "Cappuccino", "Mocha", "Latte", "Americano", "Macchiato", "Filtrado");
 	}
 }
