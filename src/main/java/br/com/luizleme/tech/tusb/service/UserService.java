067b9f677ce6df13b981620051286859f64876a5
Luiz Henrique Leme <luizleme.tech@gmail.com>
2024-10-14 21:01:36 -0300

    feat(testes-spring-boot): Implementada a classe UserService

---
+++ src/main/java/br/com/luizleme/tech/tusb/service/UserService.java
@@ -0,0 +1,17 @@
+package br.com.luizleme.tech.tusb.service;
+
+import br.com.luizleme.tech.tusb.domain.User;
+import br.com.luizleme.tech.tusb.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	public User createUser(User user) {
+		return userRepository.save(user);
+	}
+}
