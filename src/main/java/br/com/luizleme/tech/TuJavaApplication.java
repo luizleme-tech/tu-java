272af5dafc160dcb29dc5abf9303da29cc8441a8
Luiz Henrique Leme <luizleme.tech@gmail.com>
2024-10-14 21:00:52 -0300

    chore(testes-spring-boot): Adicionadas as classes de inicialização de Spring Boot

---
+++ src/main/java/br/com/luizleme/tech/TuJavaApplication.java
@@ -0,0 +1,7 @@
+package br.com.luizleme.tech;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuJavaApplication {
+}
---
+++ src/test/java/br/com/luizleme/tech/TuJavaApplicationTest.java
@@ -0,0 +1,8 @@
+package br.com.luizleme.tech;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+public class TuJavaApplicationTest {
+}
